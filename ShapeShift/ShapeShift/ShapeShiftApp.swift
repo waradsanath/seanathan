bbd4d6b82c34e9258bbcc882b16d77069963a672
waradsanath <warad_sanath_manjunath@s2023.ssts.edu.sg>
2024-07-13 14:35:34 +0800

    made app

---
+++ ShapeShift/ShapeShift/ShapeShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeShiftApp.swift
+//  ShapeShift
+//
+//  Created by Sanath Warad on 13/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ShapeShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
