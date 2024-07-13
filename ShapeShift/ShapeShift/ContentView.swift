ec41d5a2e2df8ec937a9259a48e2ea1aaded4563
Jake <tkrobot@sabook015.local>
2024-07-13 15:24:52 +0800

    changed a comment

--- ShapeShift/ShapeShift/ContentView.swift
+++ ShapeShift/ShapeShift/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            // comment
+            // lmao
         }
         .padding()
     }
