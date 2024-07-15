f4b90002a5cf4c10f01bba8f3116d0ed0067f514
James-Chew <chew.yong.hong.james@sji.edu.sg>
2024-07-16 07:24:28 +0800

    Finished second page of the second page of the launch view

--- ShapeShift/ShapeShift/FirstLaunchView.swift
+++ ShapeShift/ShapeShift/FirstLaunchView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct FirstLaunchView: View {
     @State var yWelcome = 350.0
     @State var page = 1
+    @State var yImage = 700.0
+    @State var yText2 = 700.0
     var body: some View {
         NavigationStack {
             VStack {
@@ -20,10 +22,23 @@ struct FirstLaunchView: View {
                     .frame(alignment: .center)
                     .position(x: 186, y: yWelcome)
                     .animation(.easeInOut(duration: 1), value: yWelcome)
+                Image("PersonSitting")
+                    .position(x: 186, y: yImage)
+                    .animation(.easeInOut(duration: 1), value: yImage)
+                Text("But first, lets enter your details")
+                    .font(.title)
+                    .position(x: 165, y: yText2)
+                    .animation(.easeInOut(duration: 1), value: yText2)
+                    .frame(width: 341, height:200)
+                    .multilineTextAlignment(.center)
+                    
                 Button {
                     if page == 1 {
                         yWelcome -= 300
+                        yImage -= 700
+                        yText2 -= 750
                         page += 1
+
                     }
                 } label: {
                     Text("Move")
