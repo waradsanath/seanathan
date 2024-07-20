873e1a4364db6c8c253725c3edfcc50c270c05b7
drsvds <dinguspodsg@gmail.com>
2024-07-20 17:26:56 +0800

    diet view changes

--- ShapeShift/ShapeShift/Diet view.swift
+++ ShapeShift/ShapeShift/Diet view.swift
@@ -5,4 +5,66 @@
 //  Created by T Krobot on 20/7/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct DietView: View {
+    
+    var body: some View {
+       
+        NavigationView{
+            ZStack{
+            Color.black
+                .ignoresSafeArea()
+                ScrollView{
+                    VStack(alignment: .leading, spacing: 50){
+                        
+                    Text("Limit")
+                        .foregroundColor(.white)
+                        .bold()
+                        .font(.system(size: 30))
+                        .padding()
+                        
+                        
+                        HStack{
+                            Text("Meals")
+                            
+                                .foregroundColor(.white)
+                                .bold()
+                                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                                .font(.system(size: 30))
+                                .padding()
+                            
+                            Spacer()
+                            Button {
+                            } label: {
+                                Text(Image(systemName: "plus"))
+                                    .padding()
+                            }
+                            
+                        }
+                        
+                        NavigationStack{
+                            
+                            
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Diet")
+        }
+        
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+#Preview {
+    DietView()
+}
